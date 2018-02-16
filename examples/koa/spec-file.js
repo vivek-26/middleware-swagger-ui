aff760bc064cffd56fb313a28207130ee057c074
Vivek Kumar <vivek.kumar26@live.com>
2018-02-16 20:13:51 +0530

    chore(example): add koa spec file example

---
+++ examples/koa/spec-file.js
@@ -0,0 +1,27 @@
+const Koa = require('koa');
+const app = new Koa();
+const Router = require('koa-router');
+const router = new Router();
+const { koaSwaggerUI } = require('middleware-swagger-ui');
+const PORT = 3000;
+
+/* middleware-swagger-ui options */
+const options = {
+	title: 'Swagger UI',
+	oauthOptions: false,
+	swaggerOptions: {
+		dom_id: '#swagger-ui',
+		specFile: './swagger-docs/index.json',
+		layout: 'StandaloneLayout',
+		deepLinking: true,
+	},
+	hideTopbar: false,
+};
+
+router.get('/docs*', koaSwaggerUI(options));
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT, () => {
+	console.log(`Server started on port: ${PORT}`);
+	console.log(`Swagger Docs - http://localhost:${PORT}/docs`);
+});
